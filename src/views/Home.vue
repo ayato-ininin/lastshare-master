225a160102e2b852109e69ea49e589c7650ff1d9
ayato-ininin <sarai.a.1203408@gmail.com>
2020-12-20 16:58:34 +0900

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="flex">
+
+    <div class="left">
+     <SideNavi></SideNavi>
+    </div>
+    <div class="right">
+      <div class="title">
+        <p>ホーム</p>
+      </div>
+      <Message></Message>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import SideNavi from "../components/SideNavi"
+import Message from "../components/Message"
+export default {
+  components: {
+    SideNavi,
+    Message
+  }
+};
+</script>
+
+<style scoped>
+.left{
+  width: 25%;
+  height: 100vh;
+}
+.right{
+  width: 78%;
+  height: 100vh;
+  margin-left: 30px;
+}
+.flex{
+  display: flex;
+}
+.title{
+ border-bottom: 0.1px solid white;
+  border-left: 0.1px solid white;
+  padding: 10px;
+}
+</style>
